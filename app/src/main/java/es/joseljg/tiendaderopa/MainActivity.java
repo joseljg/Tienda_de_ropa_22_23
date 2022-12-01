4ee642b8cc672f4bd85dd454fb88f5917093e0a2
joseljg <joseljg@gmail.com>
2022-12-01 19:46:14 +0100

    version 0.1

--- app/src/main/java/es/joseljg/tiendaderopa/MainActivity.java
+++ app/src/main/java/es/joseljg/tiendaderopa/MainActivity.java
@@ -6,22 +6,38 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
+import es.joseljg.tiendaderopa.clases.Ropa;
 import es.joseljg.tiendaderopa.modelo.ConfiguracionDB;
+import es.joseljg.tiendaderopa.modelo.RopaDB;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<foreach> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Connection c = ConfiguracionDB.conectarConBaseDeDatos();
+  /*      Connection c = ConfiguracionDB.conectarConBaseDeDatos();
         if(c == null)
         {
             Log.i("sql", "error al conectar");
         }
         else{
             Log.i("sql", "conectado correctamente");
+        }*/
+   /*
+    ArrayList<Ropa> ropas = RopaDB.obtenerRopas();
+    if(ropas!= null) {
+        for (Ropa r : ropas) {
+            Log.i("sql", r.toString());
+            System.out.println(r.toString());
         }
+    }
+    else{
+        Log.i("sql", "no se pudieron obtener los datos");
+        System.out.println("no se pudieron obtener los datos");
+    }
+    */
     }
 }
